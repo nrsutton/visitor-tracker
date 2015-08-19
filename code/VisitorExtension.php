6080c897498a8dbaa1e75532bf6364d89cb47670
Nick Sutton <nsutton@seriouslaw.co.uk>
2015-08-19 10:45:37 +0100

    Moved the detection code into postRequest method so I can catch 404s

--- code/VisitorExtension.php
+++ code/VisitorExtension.php
@@ -1,5 +1,5 @@
 <?php
-    class VisitorExtension extends DataExtension
+    class VisitorExtension extends DataExtension implements RequestFilter
     {
         private static $has_one = array (
             'Visitor' => 'Visitor'
@@ -14,7 +14,15 @@
 
         public function contentControllerInit( $controller )
         {
-            if ( ! Director::is_ajax() )
+        }
+
+        public function preRequest( SS_HTTPRequest $request, Session $session, DataModel $model )
+        {
+        }
+
+        public function postRequest( SS_HTTPRequest $request, SS_HTTPResponse $response, DataModel $model )
+        {
+            if  ( ! $response->isError() && ! Director::is_ajax() )
             {
                 // Find or create the visitor record
                     $visitor = Visitor::initVisitor();
