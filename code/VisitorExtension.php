cfd412f32f528e069399ee070dc6ea4d626344ca
Nick Sutton <nsutton@seriouslaw.co.uk>
2015-09-22 14:30:43 +0100

    Removed the destructor method from VisitorExtension that added javascript

--- code/VisitorExtension.php
+++ code/VisitorExtension.php
@@ -5,13 +5,6 @@
             'Visitor' => 'Visitor'
         );
 
-        function __destruct()
-        {
-            // Only way I can see of adding module javascript after the main page javascript (So after the jquery include)
-            // is to put the requirement in the __destruct
-            Requirements::javascript( "moduleVisitorTracker/javascript/visitor-tracker.js" );
-        }
-
         public function contentControllerInit( $controller )
         {
         }
